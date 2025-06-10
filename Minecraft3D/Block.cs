b448fdfeecb2cfaa7eeab9642024bdf3b15a924c
stud <stud@07-415-12>
2025-06-10 11:20:43 +0300

    Доработка класса Block

--- Minecraft3D/Block.cs
+++ Minecraft3D/Block.cs
@@ -8,5 +8,18 @@ namespace Minecraft3D
 {
     public class Block
     {
+        public int X {  get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public Block(int x, int y) 
+        {
+            X = x;
+            Y = y;
+            Z = 0;
+        }
+        public Block(int x, int y, int z) : this(x, y)
+        {
+            Z = z;
+        }
     }
 }
